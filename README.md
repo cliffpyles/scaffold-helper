9ec766503c3c09c4f9bcc65e1b17fe3eeac8f0b5
Cliff Pyles <cliff@cliffpyles.com>
2018-02-14 14:14:34 -0500

    Initialize project

---
+++ README.md
@@ -0,0 +1,134 @@
+# scaffold-helper
+
+> Copies files and folders from source directory to destination directory (all directories recursively or just files from the source directory) with template style from [template-file](https://github.com/gsandf/template-file#readme)
+
+[![Build Status](https://travis-ci.org/cliffpyles/scaffold-helper.svg?branch=master)](https://travis-ci.org/cliffpyles/scaffold-helper)
+[![Build status](https://ci.appveyor.com/api/projects/status/6xmc4f007uoacpcl?svg=true)](https://ci.appveyor.com/project/cliffpyles/scaffold-helper)
+[![Coverage Status](https://coveralls.io/repos/github/cliffpyles/scaffold-helper/badge.svg?branch=master)](https://coveralls.io/github/cliffpyles/scaffold-helper?branch=master)
+
+## Installation
+
+```sh
+$ npm i scaffold-helper --save
+```
+
+or
+
+```sh
+$ yarn add scaffold-helper
+```
+
+## Usage
+
+In this section you will see two example usages. [One example](#example-one) copies the complete source directory tree to the destination and replaces all variables within the templates. [The other example](#example-two) will only copy files within the source directory to the destination directory and replaces all variables within the templates.
+
+The given source directory tree:
+
+```
+.
++-- source/directory/
+    |
+    +-- dir-1
+    |   |
+    |   +-- file-2
+    |
+    +-- dir-2
+    |   |
+    |   +-- file-3
+    |
+    +-- template-1
+```
+
+The given `template-1`:
+
+```txt
+My name is {{name}} and I am {{age}} years old.
+```
+
+### example one
+
+The `scaffold-helper` module example **WITH** copying recursive all files and directories:
+
+```js
+const templateDir = require('scaffold-helper'); // import templateDir from 'scaffold-helper';
+
+// if we set onlyFiles to false it copies the complete directory tree
+// from 'source/directory' to 'destination/directory'
+// excluding 'dir-1'
+templateDir(
+  {
+    source: 'source/directory',
+    destination: 'destination/directory',
+    onlyFiles: false,
+    exclude: ['dir-1'], // add as many directories as you want to the array
+  },
+  {
+    name: 'Cliff',
+    age: '25',
+  },
+);
+```
+
+The example above will copy the source directory tree to the 'destination/directory' and replace all variables within all files
+
+The destination directory tree:
+
+```
+.
++-- destination/directory/
+    |
+    +-- dir-2
+    |   |
+    |   +-- file-3
+    |
+    +-- template-1
+```
+
+The template-1 with filled variables:
+
+```txt
+My name is Cliff and I am 25 years old.
+```
+
+### example two
+
+The `scaffold-helper` module example **WITHOUT** copying recursive all files and directories:
+
+```js
+const templateDir = require('scaffold-helper'); // import templateDir from 'scaffold-helper';
+
+// if we set onlyFiles to true it copies only the files
+// within 'source/directory' to 'destination/directory'
+templateDir(
+  {
+    source: 'source/directory',
+    destination: 'destination/directory',
+    onlyFiles: true
+  },
+  {
+    name: 'Cliff',
+    age: '25',
+  },
+);
+```
+
+The example above will only copy the files within the source directory and will not recursively copy all directories and files.
+
+The destination directory tree:
+
+```
+.
++-- destination/directory/
+    |
+    +-- template-1
+```
+
+The template-1 with filled variables:
+
+```txt
+My name is Cliff and I am 25 years old.
+```
+
+## LICENSE
+
+MIT © Cliff cliffpyles
---
+++ test/index.js
@@ -0,0 +1,228 @@
+import test from 'ava';
+import fs from 'fs-extra';
+import path from 'path';
+
+import templateDir from '../lib';
+
+test.before(() => {
+  const cwd = process.cwd();
+  const destinationFixtures1 = path.join(cwd, 'test', 'fixtures', 'destination1');
+
+  fs.mkdirSync(destinationFixtures1);
+});
+
+test.after.always(() => {
+  const cwd = process.cwd();
+  const destinationFixtures1 = path.join(cwd, 'test', 'fixtures', 'destination1');
+  const destinationFixtures2 = path.join(cwd, 'test', 'fixtures', 'destination2');
+  const destinationFixtures3 = path.join(cwd, 'test', 'fixtures', 'destination3');
+  const destinationFixtures4 = path.join(cwd, 'test', 'fixtures', 'destination4');
+
+  fs.removeSync(destinationFixtures1);
+  fs.removeSync(destinationFixtures2);
+  fs.removeSync(destinationFixtures3);
+  fs.removeSync(destinationFixtures4);
+});
+
+test('TEMPLATE DIR | recursively', (t) => {
+  const cwd = process.cwd();
+  const source = path.join(cwd, 'test', 'fixtures', 'source');
+  const destination = path.join(cwd, 'test', 'fixtures', 'destination1');
+  const onlyFiles = false;
+  const data = {
+    type: 'example',
+    name: 'Cliff',
+    age: '34',
+  };
+
+  templateDir({ source, destination, onlyFiles }, data);
+
+  const valueDestinationDir = fs.readdirSync(destination);
+  const expectedDestinationDir = [
+    'dir-1',
+    'dir-2',
+    'example.txt',
+    'template-1',
+  ];
+
+  const valueDestinationDir1 = fs.readdirSync(path.join(destination, 'dir-1'));
+  const expectedDestinationDir1 = [
+    'dir-3',
+    'dir-4',
+    'dir-5',
+    'file-1',
+  ];
+
+  const valueDestinationDir2 = fs.readdirSync(path.join(destination, 'dir-2'));
+  const expectedDestinationDir2 = [
+    'file-2',
+  ];
+
+  const valueDestinationDir3 = fs.readdirSync(path.join(destination, 'dir-1', 'dir-3'));
+  const expectedDestinationDir3 = [
+    'file-3',
+  ];
+
+  const valueDestinationDir4 = fs.readdirSync(path.join(destination, 'dir-1', 'dir-4'));
+  const expectedDestinationDir4 = [
+    'file-4',
+  ];
+
+  const valueDestinationDir5 = fs.readdirSync(path.join(destination, 'dir-1', 'dir-5'));
+  const expectedDestinationDir5 = [
+    'file-5',
+  ];
+
+  const valueTemplate1 = fs.readFileSync(path.join(destination, 'template-1'), 'utf-8');
+  const expectedTemplate1 = 'My name is Cliff and I am 34 years old.\n';
+
+  t.deepEqual(valueDestinationDir, expectedDestinationDir);
+  t.deepEqual(valueDestinationDir1, expectedDestinationDir1);
+  t.deepEqual(valueDestinationDir2, expectedDestinationDir2);
+  t.deepEqual(valueDestinationDir3, expectedDestinationDir3);
+  t.deepEqual(valueDestinationDir4, expectedDestinationDir4);
+  t.deepEqual(valueDestinationDir5, expectedDestinationDir5);
+  t.is(valueTemplate1, expectedTemplate1);
+});
+
+test('TEMPLATE DIR | files only', (t) => {
+  const source = path.join('test', 'fixtures', 'source');
+  const destination = path.join('test', 'fixtures', 'destination2');
+  const onlyFiles = true;
+  const data = {
+    type: 'example',
+    name: 'Cliff',
+    age: '34',
+  };
+
+  templateDir({ source, destination, onlyFiles }, data);
+
+  const valueDestinationDir = fs.readdirSync(destination);
+  const expectedDestinationDir = [
+    'example.txt',
+    'template-1',
+  ];
+
+  const valueTemplate1 = fs.readFileSync(path.join(destination, 'template-1'), 'utf-8');
+  const expectedTemplate1 = 'My name is Cliff and I am 34 years old.\n';
+
+  t.deepEqual(valueDestinationDir, expectedDestinationDir);
+  t.is(valueTemplate1, expectedTemplate1);
+});
+
+test('TEMPLATE DIR | throws', (t) => {
+  const cwd = process.cwd();
+  const wrongSource = path.join(cwd, 'test', 'fixtures', 'sourc');
+  const destination = path.join(cwd, 'test', 'fixtures', 'dest');
+
+  t.throws(() => templateDir({ source: wrongSource, destination }, {}));
+});
+
+test.serial('TEMPLATE DIR | recursively with default values', async (t) => {
+  const cwd = process.cwd();
+  const source = path.join(cwd, 'test', 'fixtures', 'source');
+  const destination = path.join(source, 'destination');
+
+  await process.chdir(source);
+
+  templateDir();
+
+  const valueDestinationDir = fs.readdirSync(destination);
+  const expectedDestinationDir = [
+    '__type__.txt',
+    'dir-1',
+    'dir-2',
+    'template-1',
+  ];
+
+  const valueDestinationDir1 = fs.readdirSync(path.join(destination, 'dir-1'));
+  const expectedDestinationDir1 = [
+    'dir-3',
+    'dir-4',
+    'dir-5',
+    'file-1',
+  ];
+
+  const valueDestinationDir2 = fs.readdirSync(path.join(destination, 'dir-2'));
+  const expectedDestinationDir2 = [
+    'file-2',
+  ];
+
+  const valueDestinationDir3 = fs.readdirSync(path.join(destination, 'dir-1', 'dir-3'));
+  const expectedDestinationDir3 = [
+    'file-3',
+  ];
+
+  const valueDestinationDir4 = fs.readdirSync(path.join(destination, 'dir-1', 'dir-4'));
+  const expectedDestinationDir4 = [
+    'file-4',
+  ];
+
+  const valueDestinationDir5 = fs.readdirSync(path.join(destination, 'dir-1', 'dir-5'));
+  const expectedDestinationDir5 = [
+    'file-5',
+  ];
+
+  const valueTemplate1 = fs.readFileSync(path.join(destination, 'template-1'), 'utf-8');
+  const expectedTemplate1 = 'My name is  and I am  years old.\n';
+
+  fs.removeSync(destination);
+
+  t.deepEqual(valueDestinationDir, expectedDestinationDir);
+  t.deepEqual(valueDestinationDir1, expectedDestinationDir1);
+  t.deepEqual(valueDestinationDir2, expectedDestinationDir2);
+  t.deepEqual(valueDestinationDir3, expectedDestinationDir3);
+  t.deepEqual(valueDestinationDir4, expectedDestinationDir4);
+  t.deepEqual(valueDestinationDir5, expectedDestinationDir5);
+  t.is(valueTemplate1, expectedTemplate1);
+
+  await process.chdir(cwd);
+});
+
+test('TEMPLATE DIR | recursively, with excluding dir-2, dir-4', (t) => {
+  const cwd = process.cwd();
+  const source = path.join(cwd, 'test', 'fixtures', 'source');
+  const destination = path.join(cwd, 'test', 'fixtures', 'destination4');
+  const onlyFiles = false;
+  const exclude = ['dir-2', 'dir-4'];
+  const data = {
+    type: 'example',
+    name: 'Cliff',
+    age: '34',
+  };
+
+  templateDir({ source, destination, onlyFiles, exclude }, data);
+
+  const valueDestinationDir = fs.readdirSync(destination);
+  const expectedDestinationDir = [
+    'dir-1',
+    'example.txt',
+    'template-1',
+  ];
+
+  const valueDestinationDir1 = fs.readdirSync(path.join(destination, 'dir-1'));
+  const expectedDestinationDir1 = [
+    'dir-3',
+    'dir-5',
+    'file-1',
+  ];
+
+  const valueDestinationDir3 = fs.readdirSync(path.join(destination, 'dir-1', 'dir-3'));
+  const expectedDestinationDir3 = [
+    'file-3',
+  ];
+
+  const valueDestinationDir5 = fs.readdirSync(path.join(destination, 'dir-1', 'dir-5'));
+  const expectedDestinationDir5 = [
+    'file-5',
+  ];
+
+  const valueTemplate1 = fs.readFileSync(path.join(destination, 'template-1'), 'utf-8');
+  const expectedTemplate1 = 'My name is Cliff and I am 34 years old.\n';
+
+  t.deepEqual(valueDestinationDir, expectedDestinationDir);
+  t.deepEqual(valueDestinationDir1, expectedDestinationDir1);
+  t.deepEqual(valueDestinationDir3, expectedDestinationDir3);
+  t.deepEqual(valueDestinationDir5, expectedDestinationDir5);
+  t.is(valueTemplate1, expectedTemplate1);
+});
