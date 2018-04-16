6f75f7c8419325f8252a22f3c4d3523db147fe8a
Cliff Pyles <cpyles@redhat.com>
2018-04-16 12:03:44 -0400

    Fix generation for custom variables and add test for generating custom variables

--- test/index.js
+++ test/index.js
@@ -7,8 +7,10 @@ import scaffold from '../lib';
 test.before(() => {
   const cwd = process.cwd();
   const destinationFixtures1 = path.join(cwd, 'test', 'fixtures', 'destination1');
+  const destinationFixturesCustom = path.join(cwd, 'test', 'fixtures', 'custom-destination');
 
   fs.mkdirSync(destinationFixtures1);
+  fs.mkdirSync(destinationFixturesCustom);
 });
 
 test.after.always(() => {
@@ -17,11 +19,13 @@ test.after.always(() => {
   const destinationFixtures2 = path.join(cwd, 'test', 'fixtures', 'destination2');
   const destinationFixtures3 = path.join(cwd, 'test', 'fixtures', 'destination3');
   const destinationFixtures4 = path.join(cwd, 'test', 'fixtures', 'destination4');
+  const destinationFixturesCustom = path.join(cwd, 'test', 'fixtures', 'custom-destination');
 
   fs.removeSync(destinationFixtures1);
   fs.removeSync(destinationFixtures2);
   fs.removeSync(destinationFixtures3);
   fs.removeSync(destinationFixtures4);
+  fs.removeSync(destinationFixturesCustom);
 });
 
 test('TEMPLATE DIR | recursively', (t) => {
@@ -224,5 +228,35 @@ test('TEMPLATE DIR | recursively, with excluding dir-2, dir-4', (t) => {
   t.deepEqual(valueDestinationDir1, expectedDestinationDir1);
   t.deepEqual(valueDestinationDir3, expectedDestinationDir3);
   t.deepEqual(valueDestinationDir5, expectedDestinationDir5);
+  t.is(valueTemplate1, expectedTemplate1);
+});
+
+test('TEMPLATE DIR | custom template variables', (t) => {
+  const source = path.join('test', 'fixtures', 'custom');
+  const destination = path.join('test', 'fixtures', 'custom-destination');
+  const onlyFiles = true;
+  const data = {
+    type: 'example',
+    name: 'Cliff',
+    age: '34',
+  };
+  const variableRegex = /\[\[\s?(.*?)\s?\]\]/g;
+
+  scaffold({
+    source,
+    destination,
+    onlyFiles,
+    variableRegex
+  }, data);
+
+  const valueDestinationDir = fs.readdirSync(destination);
+  const expectedDestinationDir = [
+    'custom-variables.txt',
+  ];
+
+  const valueTemplate1 = fs.readFileSync(path.join(destination, 'custom-variables.txt'), 'utf-8');
+  const expectedTemplate1 = 'My name is Cliff and I am 34 years old.\n';
+
+  t.deepEqual(valueDestinationDir, expectedDestinationDir);
   t.is(valueTemplate1, expectedTemplate1);
 });
